30dbe59149e016df94ed7e60d62c1d8529e7f193
tchoforwilson <tchoforwilson@gmail.com>
2024-08-17 12:15:52 +0100

    Added date created field for movie

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
@@ -10,3 +11,4 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
